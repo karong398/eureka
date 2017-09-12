80590c711fb967d03ee27b6e4b21c8c6ca26820f
卡戎 <13422353612@163.com>
2017-09-12 15:41:19 +0800

    eureka ip 显示

--- src/main/java/com/SpringCloudEurekaApplication.java
+++ src/main/java/com/SpringCloudEurekaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication
 @EnableEurekaServer //声明这是一个 Eureka Server
 public class SpringCloudEurekaApplication {
